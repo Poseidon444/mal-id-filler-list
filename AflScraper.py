44ba35bb5d45872491d1bee3583821312a5bb13b
Shivam <shivam.sdb2@gmail.com>
2022-01-23 19:33:05 +0530

    add afl scraper

---
+++ AflScraper.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import json
+
+url = "https://www.animefillerlist.com"
+page = requests.get(url+"/shows")
+
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.select("#ShowList > .Group > ul > li > a")
+
+links = {}
+
+for i in results:
+    links[re.search(".+(?=\()|.+",i.text)[0]] = url+i.attrs["href"]
+
+with open("links.json", "w") as fp:
+    json.dump(links , fp, indent = 4) 
